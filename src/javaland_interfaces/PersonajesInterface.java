d48af7c01bc991be94c9390f8c1d487774d8d197
saulGutierrezRuiz <sgutierrezr07@educantabria.es>
2026-02-05 17:17:11 +0100

    Interfaz de Personajes

--- src/javaland_interfaces/PersonajesInterface.java
+++ src/javaland_interfaces/PersonajesInterface.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
+
 package javaland_interfaces;
 
 /**
@@ -9,5 +6,27 @@ package javaland_interfaces;
  * @author cu3nt
  */
 public interface PersonajesInterface {
+    
+    //Monstruos y valientes
+    public<T> double atacar(T Personaje);
+    
+    public double recibirDaño(int cantidad);
+    
+    //Valientes
+    public void usarHabilidadEspecial(Monstruo m);
+    
+    public void subirNivel();
+    
+    public void crearValientesIniciales(int fuerza, int velocidad, String habilidad, int defensa);
+    
+    //Monstruos
+    
+    public void generarMonstruos(int nivel);
+    
+    public void eleminarMonstruo(Monstruo m);
+    
+    
+   
+    
     
 }
