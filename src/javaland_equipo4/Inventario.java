eaed26653ba55acdc893d90fa44cac5a83c7bcc5
Berdiali <dberdiali02@educantabria.es>
2026-02-09 22:35:30 +0100

    A

--- src/javaland_equipo4/Inventario.java
+++ src/javaland_equipo4/Inventario.java
@@ -4,10 +4,49 @@
  */
 package javaland_equipo4;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author diego
  */
 public class Inventario {
     
+    private ArrayList<Objeto> objetos;
+    public Inventario() {
+        objetos = new ArrayList<>();
+    }
+    public void añadirObjeto(Objeto objeto) {
+        objetos.add(objeto);
+    }
+
+    // Usar un objeto del inventario
+    public void usarObjeto(int posicion, Valiente valiente) {
+
+        if (posicion < 0 || posicion >= objetos.size()) {
+            System.out.println("Posición inválida.");
+            return;
+        }
+
+        Objeto objeto = objetos.get(posicion);
+
+        // El objeto aplica su efecto
+        objeto.equipar(valiente);
+
+        // Se elimina del inventario (consumido/equipado)
+        objetos.remove(posicion);
+    }
+
+    // Mostrar inventario
+    public void mostrarInventario() {
+
+        if (objetos.isEmpty()) {
+            System.out.println("Inventario vacío.");
+            return;
+        }
+
+        for (int i = 0; i < objetos.size(); i++) {
+            System.out.println(i + " - " + objetos.get(i));
+        }
+    }
 }
