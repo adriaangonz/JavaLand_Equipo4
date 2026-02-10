e14660799f4ba1807eb3be6062d497ed4d4b3219
AdrianGonzalez-g <cu3nta138@gmail.com>
2026-02-10 14:08:42 +0100

    Update Combate.java

--- src/javaland_equipo4/Combate.java
+++ src/javaland_equipo4/Combate.java
@@ -15,18 +15,32 @@ public class Combate implements CombateInterface {
     @Override
     public void iniciarCombate(Valiente valiente, Monstruo monstruo) {
         do {
-            double Iniciativa_Valiente = valiente.getVelocidad()*
-        } while (valiente.getVida()>0); 
+            double Iniciativa_Valiente = valiente.getVelocidad() * (0.75 + Math.random() * 0.25);
+            double Iniciativa_Monstruo = monstruo.getVelocidad() * (0.75 + Math.random() * 0.25);
+
+            if (Iniciativa_Valiente >= Iniciativa_Monstruo) { //Si la iniciativa de valiente es mayor o igual a la del monstruo, valiente ataca
+                turno(valiente, monstruo);
+                if (monstruo.getVida() > 0) {
+                    turno(monstruo, valiente); //si el monstruo sigue vivo, contraataca
+                }
+            } else { //En caso contrario, el monstruo ataca primero
+                turno(monstruo, valiente);
+                if (valiente.getVida() > 0) {
+                    turno(valiente, monstruo); //si el valiente sigue vivo, contraataca
+                }
+            }
+        } while (valiente.getVida() > 0 && monstruo.getVida() > 0);
+        combateTerminado(valiente, monstruo);//si sale del bucle, se acaba el combate
     }
 
     @Override
     public <T> void turno(T atacante, T defensor) {
-        
+
     }
 
     @Override
     public void combateTerminado(Object valiente, Object monstruo) {
-        
+
     }
-    
+
 }
