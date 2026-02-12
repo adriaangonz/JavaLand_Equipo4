2503c344fde4c15ce77c1b6e5426a72cc6b28c99
CiroGalanVertiz <152623830+CiroGalanVertiz@users.noreply.github.com>
2026-02-12 17:51:08 +0100

    a

--- src/javaland_equipo4/Juego.java
+++ src/javaland_equipo4/Juego.java
@@ -14,53 +14,56 @@ import javaland_interfaces.MapaInterface;
  *
  * @author cococ
  */
-public class Juego implements JuegoInterface,MapaInterface {
+public class Juego implements JuegoInterface, MapaInterface {
+
     private Mapa mapa;
     private Valiente valiente;
     private static int posicionX;
     private static int posicionY;
     private Random r;
     private GestorMonstruos g1;
+    private Inventario i1;
+
     @Override
     public void iniciarJuego() {
         System.out.println("EL COMPILADOR OSCURO");
-        this.posicionX=1;
-        this.posicionY=1;
-        g1=new GestorMonstruos();
-        this.valiente=creacionOEleccionValiente();
-        if(valiente!=null){
-            this.mapa= new Mapa(20,10);
+        this.posicionX = 1;
+        this.posicionY = 1;
+        g1 = new GestorMonstruos();
+        i1= new Inventario();
+        this.valiente = creacionOEleccionValiente();
+        if (valiente != null) {
+            this.mapa = new Mapa(20, 10);
             mostrarMenuPrincipal();
-        }
-        else{
+        } else {
             System.out.println("adios");
         }
-        
-        
+
     }
 
     @Override
     public Valiente creacionOEleccionValiente() {
         Scanner teclado = new Scanner(System.in);
-        this.valiente=null;
-        int opcion=0;
-        do{
+        this.valiente = null;
+        int opcion = 0;
+        do {
             System.out.println("1 - Crear Valiente");
             System.out.println("2 - Usar valientes Iniciales");
             System.out.println("3 - Salir");
             System.out.print("Elige una opcion:");
-            opcion=teclado.nextInt();
-            switch(opcion){
-            case 1->this.valiente = new Valiente();
-            case 2->GestoresInterface.crearValientesIniciales();
-            default->{
-                System.out.println("eso no es una opcion");
+            opcion = teclado.nextInt();
+            switch (opcion) {
+                case 1 ->
+                    this.valiente = new Valiente();
+                case 2 ->
+                    GestoresInterface.crearValientesIniciales();
+                default -> {
+                    System.out.println("eso no es una opcion");
+                }
             }
-        }
-        }while(opcion!=3);
+        } while (opcion != 3);
         return valiente;
-        
-        
+
     }
 
     @Override
@@ -70,152 +73,152 @@ public class Juego implements JuegoInterface,MapaInterface {
 
     @Override
     public void mostrarMenuPrincipal() {
-        
+
         Scanner teclado = new Scanner(System.in);
-        int opcion=0;
-        do{
+        int opcion = 0;
+        do {
             System.out.println("1 - Mostrar Valiente");
             System.out.println("2 - Equipar Objeto");
             System.out.println("3 - Mostrar mapa");
             System.out.println("4 - Moverse");
             System.out.println("5 - Salir del juego");
             System.out.print("Elige una opcion:");
-            opcion=teclado.nextInt();
-            switch(opcion){
-            case 1->mostrarValiente();
-            case 2->equiparObjeto();
-            case 3->mostrarMapa();
-            case 4->explorarMapa();
-            default->{
-                System.out.println("eso no es una opcion");
+            opcion = teclado.nextInt();
+            switch (opcion) {
+                case 1 ->
+                    mostrarValiente();
+                case 2 ->
+                    equiparObjeto();
+                case 3 ->
+                    mostrarMapa();
+                case 4 ->
+                    explorarMapa();
+                default -> {
+                    System.out.println("eso no es una opcion");
+                }
             }
-        }
-        }while(opcion!=5);
-        
-        
+        } while (opcion != 5);
+
     }
-    private void mostrarValiente(){
+
+    private void mostrarValiente() {
         System.out.println(this.valiente.toString());
     }
-    private void equiparObjeto(){
+
+    private void equiparObjeto() {
         Scanner teclado = new Scanner(System.in);
         Inventario.mostrarInventario();
-        String objeto= teclado.nextLine();
-        Inventario.usarObjeto(posicion,this.valiente);
+        String objeto = teclado.nextLine();
+        Inventario.usarObjeto(posicion, this.valiente);
     }
-    private void mostrarMapa(){
+
+    private void mostrarMapa() {
         for (int i = 0; i < this.mapa.getAlto(); i++) {
             for (int j = 0; j < this.mapa.getAncho(); j++) {
-                if(casillasAdyacentes(j,i)){
+                if (casillasAdyacentes(j, i)) {
                     System.out.print(this.mapa.getCasillas()[i][j]);
-                }
-                else{
+                } else {
                     System.out.print("[x]");
                 }
-                
+
             }
             System.out.println();
-    }
-    }
-        
-   
-    public boolean movimientoValido(int coordenada,int direccion){
-        boolean validacion;
-        if(coordenada==1){
-            //Y-1
-            if(direccion==1){
-                validacion = posicionY-1>=0;
-            }
-            //Y+1
-            else{
-                validacion= posicionY<mapa.getAlto()-1;
-            }
         }
-        else{
-            //X+1
-            if(direccion==1){
-                validacion= posicionX<mapa.getAncho()-1;
-                
+    }
+
+    public boolean movimientoValido(int coordenada, int direccion) {
+        boolean validacion;
+        if (coordenada == 1) {
+            //Y-1
+            if (direccion == 1) {
+                validacion = posicionY - 1 >= 0;
+            } //Y+1
+            else {
+                validacion = posicionY < mapa.getAlto() - 1;
             }
-            //X-1
-            else{
-                validacion = posicionX-1>=0;
+        } else {
+            //X+1
+            if (direccion == 1) {
+                validacion = posicionX < mapa.getAncho() - 1;
+
+            } //X-1
+            else {
+                validacion = posicionX - 1 >= 0;
             }
         }
         return validacion;
     }
 
     @Override
-    public boolean casillasAdyacentes(int fila,int columna) {
-        return posicionY+1==fila || posicionY-1==fila || posicionX+1==columna || posicionX-1==columna;
+    public boolean casillasAdyacentes(int fila, int columna) {
+        return posicionY + 1 == fila || posicionY - 1 == fila || posicionX + 1 == columna || posicionX - 1 == columna;
     }
 
     @Override
     public void explorarMapa() {
         mostrarMapa();
         Scanner teclado = new Scanner(System.in);
-        String opcion="";
-            System.out.println("¿Hacia que direccion quieres moverte?: W/A/S/D");
-            opcion=teclado.next().substring(0,1);
-            switch(opcion.toLowerCase()){
-            case "w"->{
-                if(movimientoValido(1,1)){
-                    mapa.setCasilla(posicionY,posicionX,"[ ]");
+        String opcion = "";
+        System.out.println("¿Hacia que direccion quieres moverte?: W/A/S/D");
+        opcion = teclado.next().substring(0, 1);
+        switch (opcion.toLowerCase()) {
+            case "w" -> {
+                if (movimientoValido(1, 1)) {
+                    mapa.setCasilla(posicionY, posicionX, "[ ]");
                     posicionY--;
-                    
-                }
-                else{
+
+                } else {
                     System.out.println("no se puede mover");
                 }
-               
+
             }
-            case "a"->{
-                if(movimientoValido(-1,-1)){
-                    mapa.setCasilla(posicionY,posicionX,"[ ]");
+            case "a" -> {
+                if (movimientoValido(-1, -1)) {
+                    mapa.setCasilla(posicionY, posicionX, "[ ]");
                     posicionX--;
-                }
-                else{
+                } else {
                     System.out.println("no se puede mover");
                 }
-               
+
             }
-            case "s"->{
-                if(movimientoValido(1,-1)){
-                    mapa.setCasilla(posicionY,posicionX,"[ ]");
+            case "s" -> {
+                if (movimientoValido(1, -1)) {
+                    mapa.setCasilla(posicionY, posicionX, "[ ]");
                     posicionY++;
-                }
-                else{
+                } else {
                     System.out.println("no se puede mover");
                 }
-                
+
             }
-            case "d"->{
-                if(movimientoValido(-1,1)){
-                    mapa.setCasilla(posicionY,posicionX,"[ ]");
+            case "d" -> {
+                if (movimientoValido(-1, 1)) {
+                    mapa.setCasilla(posicionY, posicionX, "[ ]");
                     posicionX++;
-                }
-                else{
+                } else {
                     System.out.println("no se puede mover");
                 }
             }
-            default->{
+            default -> {
                 System.out.println("eso no es una opcion");
             }
-            
+
+        }
+        if (mapa.getCasillas()[posicionX][posicionY].equals("[?]")) {switch (r.nextInt(3)) {
+                case 0 ->
+                    i1.agregarObjeto(new Arma("Espada","Arma",15));
+                case 1 ->
+                    i1.agregarObjeto(new Escudo("Escudo","Escudo",15));
+                default ->
+                    i1.agregarObjeto(new Planta("Laurel","Planta",15));
+            };
             
             
         }
-            if(mapa.getCasillas()[posicionX][posicionY].equals("[?]")){
-                
-            }
-            if(mapa.getCasillas()[posicionX][posicionY].equals("[!]")){
-                g1.generarMonstruos(r.nextInt((valiente.getNivel() + 5) - Math.max(1, valiente.getNivel() - 5) + 1) + Math.max(1, valiente.getNivel() - 5));
+        if (mapa.getCasillas()[posicionX][posicionY].equals("[!]")) {
+            g1.generarMonstruos(r.nextInt((valiente.getNivel() + 5) - Math.max(1, valiente.getNivel() - 5) + 1) + Math.max(1, valiente.getNivel() - 5));
 
-            }
-        
+        }
+
     }
-    
 
-   
-    
 }
